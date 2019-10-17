0718880dc98da35316d7cabbe14dd4e66cf5a2c5
Idelfrides Jorge <idelfridesjorge@alu.ufc.br>
2019-10-17 15:01:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,24 +9,13 @@ from handle_json_files import Solutions as sol
 import time as t
 
 
-""" 
- This method is main one. It start and 
- controle this app
-"""
-
-
 def main_test():
-
-    # metodo clear aqui
-    """ crearing a object of modules:
-     -> Solutions as sol
-    """
+   """ This method is main one. It start and controle this app """
 
     sol_ob = sol.Solutions()
     choice = sol_ob.menu()
 
     if choice == 0:  # quit
-
         quit_info = """ 
     ---------------------------------------
         You chose to quit app. 
@@ -43,18 +32,17 @@ def main_test():
     else:
         pass
 
-    data_var, data_list = sol_ob.smart_solution()
+    data_variable, data_list = sol_ob.smart_solution()
 
-    if choice == 1:   # short solution
+    if choice == 1: 
         root_path = os.getcwd()
-
-        sol_ob.file_text(data_var, root_path)
-        main_test()    # call the main method again to run the app
-    elif choice == 2:  # long solution
-        sol_ob.set_type(data_var)
+        sol_ob.file_text(data_variable, root_path)
+        main_test()   
+    elif choice == 2:  
+        sol_ob.set_type(data_variable)
         main_test()
     elif choice == 3:
-        sol_ob.dict_type(data_var)
+        sol_ob.dict_type(data_variable)
         main_test()
     elif choice == 4:
         sol_ob.tuple_type(data_list)
@@ -63,7 +51,7 @@ def main_test():
         sol_ob.list_type(data_list)
         main_test()
     elif choice == 6:
-        sol_ob.variable_type(data_var)
+        sol_ob.variable_type(data_variable)
         main_test()
     else:
         pass
