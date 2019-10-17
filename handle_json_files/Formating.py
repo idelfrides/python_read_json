7ec11fac0a5187b415ba6b037b9f6401f3211ffd
Idelfrides Jorge <idelfridesjorge@alu.ufc.br>
2019-10-17 14:49:29 -0300

    Update Formating.py

--- handle_json_files/Formating.py
+++ handle_json_files/Formating.py
@@ -1,24 +1,26 @@
 
-# class contain methods to help solving
-# this issue
+""" class contain methods to help solving  this issue """
+
 import os
 
 
 class Formating(object):
+    """ Formatin data class """
 
-    # thunder init do nothing
     def __init__(self):
+        """  thunder init do nothing """
         pass
 
-    # this method formate all content to an
-    # pyhon variable. used on the smart solution
+
     def formate_all(self, content):
+        """ This method formate all content to an pyhon variable.
+        It's only used in the smart solution """
+        
         key_help = ''
         list_help = []
         list_help_one = []
         try:
             for c in content:  # c = caracter
-                # count = count + 1
                 key_help = key_help + c
                 list_help.append(c)
 
@@ -27,63 +29,6 @@ class Formating(object):
 
         except Exception as error:
             print('\n Error. Python said: {}'.format(error))
-
-    # this method formate all keys to a list
-    # used on the long solution
-    def format_multi_key(self, key, content, st, list_help):
-        key_help = ''
-        # list_help = []
-        count = st
-        try:
-            for c in content[st:]:  # c = caracter
-                count = count + 1
-                key_help = key_help + c
-                key_help_len = key_help.__len__()
-                key_len = key.__len__()
-                if key_help_len == key_len:
-                    # remove left white space
-                    key_help = key_help.lstrip()
-                    if key_help == key:
-                        list_help.append(key_help)
-                        break
-                    else:
-                        continue
-                else:
-                    pass
-            return list_help, count
-        except Exception as error:
-            print('\n Error. Python said: {}'.format(error))
-
-    # this method formate all values to the same list
-    # used on the long solution
-    def format_values_all(self, value, content, st, list_help):
-        key_help = ''
-        # list_help = []
-        count = st
-        try:
-            for caracter in content[st:]:
-                count = count + 1
-                key_help = key_help + caracter
-
-                key_help_len = key_help.__len__()
-                key_len = value.__len__()
-
-                if key_help_len == key_len:
-                    # remove left white space
-                    key_help = key_help.lstrip()
-
-                    if key_help == value:
-                        list_help.append(key_help)
-                        break
-                    else:
-                        continue
-                else:
-                    pass
-            return list_help, count
-
-        except Exception as error:
-            print('\n Error. Python said: {}'.format(error))
-
 
 
     def change_dir(self, code, root_path):
